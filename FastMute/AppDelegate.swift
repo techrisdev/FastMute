42caa7820a215a776967b6e6d27e3f6889c8747d
techrisdev <techrisdev>
2021-05-18 09:54:34 +0200

    Added a Keyboard Shortcut.

--- FastMute/AppDelegate.swift
+++ FastMute/AppDelegate.swift
@@ -3,20 +3,25 @@
 // Created by TeChris on 18.05.21.
 
 import Cocoa
-import SwiftUI
+import Carbon.HIToolbox.Events
 
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
 	let statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
 
 	func applicationDidFinishLaunching(_ aNotification: Notification) {
-		
 		if manager.isMuted {
 			statusItem.button?.title = "􀊣􀊳"
 		} else {
 			statusItem.button?.title = "􀊡􀊱"
 		}
 		statusItem.button?.action = #selector(buttonAction)
+		
+		// Setup keyboard shortcuts.
+		KeyboardShortcutManager(keyboardShortcut: KeyboardShortcut(key: Key(keyCode: UInt32(kVK_ANSI_M)), modifiers: [.control, .option], events: [.keyDown]))
+			.startListeningForEvents { _ in
+				self.buttonAction()
+			}
 	}
 	
 	let manager = VolumeManager()
